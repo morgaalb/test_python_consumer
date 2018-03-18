dafa1e750da8a13475ccbeae94e53117ced273a2
Albert Morgan <morgaalb@oregonstate.edu>
2018-03-17 20:47:11 -0700

    Added requirements file

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="test_python_consumer",
+    name="test_python_consumer3",
     version="0.2",
     scripts=["test_python_consumer_script.py"],
     dependency_links=[
+        #"https://github.com/morgaalb/test_python_package/tarball/master@master#egg=test_python_package-0.2"
         "git+https://github.com/morgaalb/test_python_package.git#egg=test_python_package-0.2"
     ],
     install_requires=["test_python_package>=0.2"]
