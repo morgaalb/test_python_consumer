3410851da77fea7f0e13d4058bdc57e7255d9015
Albert Morgan <morgaalb@oregonstate.edu>
2018-03-17 19:53:14 -0700

    updated version

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="test_python_consumer",
-    version="0.1",
+    version="0.2",
     scripts=["test_python_consumer_script.py"],
     dependency_links=[
-        "git+https://github.com/morgaalb/test_python_package.git"
+        "git+https://github.com/morgaalb/test_python_package.git#egg=test_python_package-0.2"
     ],
+    install_requires=["test_python_package>=0.2"]
 )
